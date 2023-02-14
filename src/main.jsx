751a486f40cf28e1364aa520b70e824f2ddf6365
Ericcost <eric.costerousse@laposte.net>
2023-02-14 10:45:06 +0100

    add ContextProviders

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
+import { AuthContextProvider } from './context/authContext';
 import { PostProvider } from './context/postContext'
+import { DarkModeContextProvider } from './context/darkModeContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <PostProvider>
-      <App />
-    </PostProvider>
+      <AuthContextProvider>
+        <PostProvider>
+          <App />
+        </PostProvider>
+      </AuthContextProvider>
   </React.StrictMode>,
 );
