91cd3c900ff45ad6b83733d5202353be9b320ac5
Ericcost <eric.costerousse@laposte.net>
2023-02-14 10:45:44 +0100

    New Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,15 +5,18 @@ import { useContext } from 'react';
 import { AiTwotoneHome } from "react-icons/ai";
 import { RiMoonLine, RiAppsLine } from "react-icons/ri";
 import { SlMagnifier } from "react-icons/sl";
-import { BsPersonFill } from "react-icons/bs";
+import { BsPersonFill, BsSun } from "react-icons/bs";
 import { MdMailOutline } from "react-icons/md";
 import { TbBell } from "react-icons/tb";
 import "./navbar.scss";
 
 //context
-import { DarkModeContext } from '../../context/darkModeContext';
+import { AuthContext } from '../../context/authContext';
 
 function Navbar() {
+
+  const { currentUser } = useContext(AuthContext)
+
   return (
     <div className="navbar">
       <div className="left">
@@ -34,8 +37,8 @@ function Navbar() {
         <MdMailOutline />
         <TbBell />
         <div className='user'>
-          <img src="/src/assets/atr.png" alt="John Doe fait l'arbre droit dans les chaînes du puy de dôme sur un manteau de neige" />
-          <span>John Doe</span>
+          <img src={currentUser.profilePic} alt="John Doe fait l'arbre droit dans les chaînes du puy de dôme sur un manteau de neige" />
+          <span>{currentUser.name}</span>
         </div>
       </div>
     </div>
