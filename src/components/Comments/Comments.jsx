ad26c8a91ab5905ca939161745e96c450e796c9b
Ericcost <eric.costerousse@laposte.net>
2023-02-14 10:41:13 +0100

    add Comments component

---
+++ src/components/Comments/Comments.jsx
@@ -0,0 +1,40 @@
+import "./comments.scss";
+
+function Comments() {
+
+  //Temporary
+  const comments = [
+    {
+      id: 1,
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidid",
+      name: "John Doe",
+      userId: 1,
+      profilePic: "src/assets/atr.png",
+    },
+
+    {
+      id: 2,
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidid",
+      name: "Jasmine Doe",
+      userId: 2,
+      profilePic: "src/assets/mars.png",
+    }
+  ]
+
+  return (
+    <div className="comments">
+      {comments.map((comment) => (
+        <div className="comment">
+          <img src={comment.profilePic} alt={comment.name} />
+          <div className="info">  
+            <span>{comment.name} </span>
+            <p>{comment.desc}</p>
+          </div>
+          <span className="date">1 hour ago</span>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Comments;
