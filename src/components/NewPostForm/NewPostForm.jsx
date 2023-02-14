4c5a39216af1f9d2e306f324792cb6e824461984
Ericcost <eric.costerousse@laposte.net>
2023-02-14 10:46:03 +0100

    New Post Form components

--- src/components/NewPostForm/NewPostForm.jsx
+++ src/components/NewPostForm/NewPostForm.jsx
@@ -7,9 +7,9 @@ export const NewPostForm = () => {
   const postsStore = usePostsStore();
 
   return (
-    <>
+    <div className='write'>
       <input type="text" placeholder="Write a post..." value={postText} onChange={(e) => setPostText(e.target.value)}/>
       <button onClick={() => postsStore.addPost(postText)}>New Post</button>
-    </>
+    </div>
   )
 }
